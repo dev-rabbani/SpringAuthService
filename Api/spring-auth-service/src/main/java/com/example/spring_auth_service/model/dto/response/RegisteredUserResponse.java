f56e30bd93a5288466995533177a44b4baf2c026
Pial Kanti Samadder <pialkanti2012@gmail.com>
2025-03-07 22:18:38 +0600

    Removed unused imports

--- Api/spring-auth-service/src/main/java/com/example/spring_auth_service/model/dto/response/RegisteredUserResponse.java
+++ Api/spring-auth-service/src/main/java/com/example/spring_auth_service/model/dto/response/RegisteredUserResponse.java
@@ -2,8 +2,6 @@ package com.example.spring_auth_service.model.dto.response;
 
 import lombok.Builder;
 
-import java.time.LocalDate;
-
 @Builder
 public record RegisteredUserResponse(String username,
                                      String email) {
