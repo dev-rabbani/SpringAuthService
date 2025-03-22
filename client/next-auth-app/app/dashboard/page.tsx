26272b15fdf17a65deab922e186d1844e9ade62d
dev-rabbani <rabbani.cse.69@gmail.com>
2025-03-22 23:01:30 +0600

    feat: Create initial dashboard view

---
+++ client/next-auth-app/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Typography } from '@/components/typography';
+
+export default function Page() {
+  return (
+    <div className="bg-slate-300 py-20">
+      <div className="container">
+        <Typography size="h2">Welcome to dashboard</Typography>
+      </div>
+    </div>
+  );
+}
